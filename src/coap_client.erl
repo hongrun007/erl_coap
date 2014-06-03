b0d1b8bbdc79ccbd4eba63c3e66f88c317ea17c5
PH@ubuntu <hongrun007@gmail.com>
2014-06-03 15:38:04 +0200

    change put,get,delete funcs to 3 parameters

--- src/coap_client.erl
+++ src/coap_client.erl
@@ -1,5 +1,5 @@
 -module(coap_client).
--export([get/2,get/3,get/4,put/3,delete/2,subscribe/2]).
+-export([get/2,get/3,get/4,put/3,delete/3]).
 -include("coap.hel").
 %-define(MAX_ID, 65536).
 %-define(TOKEN_LENGTH, 8).
@@ -35,7 +35,53 @@ get(Host, URI, Para) ->
 			{error, Reason}
 	end.
 	
+put(Host, URI, Para) ->
+	Token = make_token(),
+	ID = make_message_id(),
+	{ok, PDU} = pdu:make_pdu(0, ?COAP_PUT, Token, ID, URI),
+	{ok, Newpdu} = getpara(PDU, Para),
+	{ok, Destaddr} = inet_parse:address(Host),
+	case gen_udp:open(?TMP_PORT, [binary, inet, {active, false}]) of
+		{ok, Socket} ->
+			gen_udp:send(Socket, Destaddr, ?PORT, Newpdu),
+			Res =  case gen_udp:recv(Socket, 0, 6000) of
+				{ok, {Destaddr, ?PORT, Packet}} ->
+					{ok, Ver, Type, Tkl, Code, MID} = pdu:get_header(Packet),
+					io:format("~p~n", [{ok, Ver, Type, Tkl, Code, MID}]),
+					{ok, Content} = pdu:get_content(Packet),
+					io:format("value is: ~p~n", [Content]);
+				{error, Reason} ->
+					{error, Reason}
+			end,
+			gen_udp:close(Socket),
+			Res;
+		{error, Reason} ->
+			{error, Reason}
+	end.
 
+delete(Host, URI, Para) ->
+	Token = make_token(),
+	ID = make_message_id(),
+	{ok, PDU} = pdu:make_pdu(0, ?COAP_DELETE, Token, ID, URI),
+	{ok, Newpdu} = getpara(PDU, Para),
+	{ok, Destaddr} = inet_parse:address(Host),
+	case gen_udp:open(?TMP_PORT, [binary, inet, {active, false}]) of
+		{ok, Socket} ->
+			gen_udp:send(Socket, Destaddr, ?PORT, Newpdu),
+			Res =  case gen_udp:recv(Socket, 0, 6000) of
+				{ok, {Destaddr, ?PORT, Packet}} ->
+					{ok, Ver, Type, Tkl, Code, MID} = pdu:get_header(Packet),
+					io:format("~p~n", [{ok, Ver, Type, Tkl, Code, MID}]),
+					{ok, Content} = pdu:get_content(Packet),
+					io:format("value is: ~p~n", [Content]);
+				{error, Reason} ->
+					{error, Reason}
+			end,
+			gen_udp:close(Socket),
+			Res;
+		{error, Reason} ->
+			{error, Reason}
+	end.
 
 get(Host, URI) ->
     Token = make_token(),
@@ -79,57 +125,57 @@ get(Host, URI, URI_HOST, URI_PORT) ->
 			{error, Reason}
 	end.
 
-put(Host, URI, Val) ->
-	Token = make_token(),
-	ID = make_message_id(),
-	{ok,PDU} = pdu:make_pdu(0, ?COAP_PUT, Token, ID, URI),						%2 -> COAP_CODE_PUT
-	{ok,Address} = inet_parse:address(Host),
-	{ok,NewPDU} = pdu:add_payload(PDU, Val, lists:flatlength(Val)),
-	case gen_udp:open(?TMP_PORT, [binary, inet, {active, false}]) of
-		{ok,Socket} ->
-			gen_udp:send(Socket, Address, ?PORT, NewPDU),
-			Res = case gen_udp:recv(Socket, 0, 3000) of					%timeout 3000 ms
-				{ok, {Address, ?PORT, Packet}} ->
-					pdu:get_header(Packet);
-				{error, Reason} ->
-					{error, Reason}
-			end,
-			gen_udp:close(Socket),
-			Res;
-		{error, Reason} ->
-			{error,Reason}
-	end.
+%put(Host, URI, Val) ->
+%	Token = make_token(),
+%	ID = make_message_id(),
+%	{ok,PDU} = pdu:make_pdu(0, ?COAP_PUT, Token, ID, URI),						%2 -> COAP_CODE_PUT
+%	{ok,Address} = inet_parse:address(Host),
+%	{ok,NewPDU} = pdu:add_payload(PDU, Val, lists:flatlength(Val)),
+%	case gen_udp:open(?TMP_PORT, [binary, inet, {active, false}]) of
+%		{ok,Socket} ->
+%			gen_udp:send(Socket, Address, ?PORT, NewPDU),
+%			Res = case gen_udp:recv(Socket, 0, 3000) of					%timeout 3000 ms
+%				{ok, {Address, ?PORT, Packet}} ->
+%					pdu:get_header(Packet);
+%				{error, Reason} ->
+%					{error, Reason}
+%			end,
+%			gen_udp:close(Socket),
+%			Res;
+%		{error, Reason} ->
+%			{error,Reason}
+%	end.
 
-delete(Host, URI) ->
-	Token = make_token(),
-	ID = make_message_id(),
-	{ok,PDU} = pdu:make_pdu(0, ?COAP_DELETE, Token, ID, URI),
-	{ok,Address} = inet_parse:address(Host),
-	case gen_udp:open(?TMP_PORT, [binary, inet, {active, false}]) of
-		{ok,Socket} ->
-			gen_udp:send(Socket, Address, ?PORT, PDU),
-			Res = case gen_udp:recv(Socket, 0, 3000) of
-				{ok, {Address, ?PORT, Packet}} ->
-					pdu:get_header(Packet);
-				{error, Reason} ->
-					{error, Reason}
-			end,
-			gen_udp:close(Socket),
-			Res;
-		{error, Reason} ->
-			{error,Reason}
-	end.
+%delete(Host, URI) ->
+%	Token = make_token(),
+%	ID = make_message_id(),
+%	{ok,PDU} = pdu:make_pdu(0, ?COAP_DELETE, Token, ID, URI),
+%	{ok,Address} = inet_parse:address(Host),
+%	case gen_udp:open(?TMP_PORT, [binary, inet, {active, false}]) of
+%		{ok,Socket} ->
+%			gen_udp:send(Socket, Address, ?PORT, PDU),
+%			Res = case gen_udp:recv(Socket, 0, 3000) of
+%				{ok, {Address, ?PORT, Packet}} ->
+%					pdu:get_header(Packet);
+%				{error, Reason} ->
+%					{error, Reason}
+%			end,
+%			gen_udp:close(Socket),
+%			Res;
+%		{error, Reason} ->
+%			{error,Reason}
+%	end.
 
-subscribe(Host, URI) ->
-	Token = make_token(),
-	ID = make_message_id(),
-	{ok,PDU} = pdu:make_pdu(16#10, ?COAP_GET, Token, ID, URI),
-	{ok,Address} = inet_parse:address(Host),
-	{ok,NewPDU} = pdu:add_option(PDU, 6, 0, ""),						%COAP_OPTION_OBSERVE=6, length=0 , value=empty
-	Pid = spawn(fun recv_subscribe/0),
-	register(sub, Pid),
-	io:format("Parent PID is: ~p~n",[self()]),
-	Pid ! { self(), Address, NewPDU }.
+%subscribe(Host, URI) ->
+%	Token = make_token(),
+%	ID = make_message_id(),
+%	{ok,PDU} = pdu:make_pdu(16#10, ?COAP_GET, Token, ID, URI),
+%	{ok,Address} = inet_parse:address(Host),
+%	{ok,NewPDU} = pdu:add_option(PDU, 6, 0, ""),						%COAP_OPTION_OBSERVE=6, length=0 , value=empty
+%	Pid = spawn(fun recv_subscribe/0),
+%	register(sub, Pid),
+%	io:format("Parent PID is: ~p~n",[self()]),
+%	Pid ! { self(), Address, NewPDU }.
 %	loop().
 %	recv_subscribe(self(), Address, NewPDU).
 %	case gen_udp:open(0, [binary, inet, {active, true}]) of			%use random UDP port number
